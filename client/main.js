5da096ce58a349eb487747eafe10dd971a68ffcc
yomi4486 <105367179+yomi4486@users.noreply.github.com>
2025-02-24 17:51:23 +0900

    Add: Discord SDK

--- client/main.js
+++ client/main.js
@@ -1,5 +1,20 @@
-import './style.css'
-import rocketLogo from '/rocket.png'
+// SDK をインポート
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import "./style.css";
+import rocketLogo from '/rocket.png';
+
+// SDK のインスタンスを生成
+const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+setupDiscordSdk().then(() => {
+  console.log("Discord SDK is ready");
+});
+
+// SDK を介してアプリにアクセス
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+}
 
 document.querySelector('#app').innerHTML = `
   <div>
